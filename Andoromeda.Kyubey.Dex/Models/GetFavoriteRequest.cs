038d74dc5d5a5e958ba2988c6a633590e9bd8f48
vist0r J <402692034@qq.com>
2018-11-30 17:16:22 +0800

    fixformat

--- Andoromeda.Kyubey.Dex/Models/GetFavoriteRequest.cs
+++ Andoromeda.Kyubey.Dex/Models/GetFavoriteRequest.cs
@@ -8,8 +8,11 @@ namespace Andoromeda.Kyubey.Dex.Models
     public class GetFavoriteRequest
     {
         public string symbol { get; set; } 
+
         public double unit_price { get; set; }
+
         public double change { get; set; }
+
         public bool favorite { get; set; }
     }
 }
--- Andoromeda.Kyubey.Dex/Models/HistoryOrders.cs
+++ Andoromeda.Kyubey.Dex/Models/HistoryOrders.cs
@@ -8,13 +8,21 @@ namespace Andoromeda.Kyubey.Dex.Models
     public class HistoryOrders
     {
         public string id     { get; set; }
+
         public string symbol { get; set; }
+
         public string bidder { get; set; }
+
         public string asker  { get; set; }
+
         public string type   { get; set; }
+
         public double unit_price { get; set; }
-        public double amount { get; set; }
+
+        public double amount { get; set;}
+
         public double total  { get; set; }
+
         public DateTime time   { get; set; }
     }
 }
