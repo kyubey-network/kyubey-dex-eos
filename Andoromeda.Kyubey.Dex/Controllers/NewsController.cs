6a8ac2de3ddef10a8f5c7806f426ba564d3cbff6
qinxiaowen <453000697@qq.com>
2018-11-30 11:34:42 +0800

    news api v0.2

--- Andoromeda.Kyubey.Dex/Controllers/NewsController.cs
+++ Andoromeda.Kyubey.Dex/Controllers/NewsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Andoromeda.Kyubey.Dex.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Andoromeda.Kyubey.Dex.Controllers
@@ -11,6 +12,13 @@ namespace Andoromeda.Kyubey.Dex.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+
+
+        [HttpGet("api/v1/lang/{lang}/news?skip={skip}&take={take}&type={type}")]
+        public IActionResult Get([FromServices] NewsRepositoryFactory newsRepositoryFactory) {
+
         }
     }
 }
---
+++ Andoromeda.Kyubey.Dex/Models/GetPagingRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Andoromeda.Kyubey.Dex.Models
+{
+    public class GetPagingRequest
+    {
+        public int Skip { get; set; }
+        public int Take { get; set; }
+    }
+}
