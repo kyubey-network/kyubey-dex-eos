75eb8231f753587c500f16db06f848bf6a51d397
Yuko Zheng <1@1234.sh>
2018-12-10 05:04:13 +0000

    Merged PR 81: Implement wallet UI (UI only)
    Implement wallet UI (UI only)
    
    TODOs:
    - i18n
    - Remove unused css
    - Integrate with data
    
    @<t-sunhaozhe> Pls help finish the todo list. Thanks.

--- Andoromeda.Kyubey.Dex/wwwroot/js/kyubey.routing.js
+++ Andoromeda.Kyubey.Dex/wwwroot/js/kyubey.routing.js
@@ -1,10 +1,11 @@
 ﻿LazyRouting.SetRoute({
     '/home': null,
+    '/wallet': null,
     '/exchange/:id': { id: '[A-Z]{1,10}' },
     '/token/all': null,
     '/token/:id': { id: '[A-Z]{1,10}' },
     '/home/contact': null,
-    '/home/news': null,
+    '/home/news': null
 });
 
 LazyRouting.SetMirror({
---
+++ Andoromeda.Kyubey.Dex/wwwroot/views/wallet/index.js
@@ -0,0 +1,24 @@
+﻿component.data = function () {
+    return {
+        tokens: [],
+        searchText: ''
+    };
+};
+
+component.computed = {
+    totalEvaluated: function () {
+        if (!this.tokens.length) {
+            return 0;
+        }
+        var values = this.tokens.map(x => x.evaluated);
+        return values.reduce(function (prev, curr) {
+            return prev + curr;
+        });
+    }
+};
+
+component.created = function () {
+    if (!app.isSignedIn) {
+        app.redirect('/');
+    }
+};
